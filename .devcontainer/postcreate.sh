7d80da6101fb68e45b8dc2cd303dd561f3c32372
lindjacob <jacob.lind2@gmail.com>
2024-07-09 11:17:42 +0000

    Issue #3 - Added pipenv + argparse + pytest + set remoteUser

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,4 +1,13 @@
 #!/bin/env bash
-
 git config --global --add safe.directory "$(git rev-parse --show-toplevel)"
 git config --local --get include.path | grep -e ../.gitconfig || git config --local --add include.path ../.gitconfig
+
+# Instruct the container to always load your environment on startup
+pushd "$(git rev-parse --show-toplevel)" || exit 1
+if [ -f "requirements.txt" ];then 
+  pip3 install --break-system-packages --user -r requirements.txt || echo No "requirements.txt" file
+fi
+if [ -f "Pipfile" ];then
+  pipenv install || echo no "Pipfile" file
+fi
+popd || exit 1
